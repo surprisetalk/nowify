2546593e89c3cb12fd2e6d00a55770e7c26315bd
Taylor Troesh <surprisetalk@gmail.com>
2023-06-13 22:07:24 -0700

    💴

--- nowify.ts
+++ nowify.ts
@@ -1,12 +1,10 @@
-//
 ////////////////////////////////////////////////////////////////////////////////
 
-import { parse as parseArgs } from "https://deno.land/std/flags/mod.ts"
+import { parse as parseArgs } from "https://deno.land/std/flags/mod.ts";
 
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
 import { readKeypress } from "https://deno.land/x/keypress@0.0.11/mod.ts";
-
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -24,14 +22,13 @@ const { _: args, help } = parseArgs(
       help: false,
       config: `TODO`,
     },
-    boolean: [ "help" ],
-    string: [ "config" ],
+    boolean: ["help"],
+    string: ["config"],
     alias: { h: "help", c: "config" },
-  }
+  },
 );
 
 const command = args.join(` `).trim();
-
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -45,72 +42,76 @@ db.execute(`
   , action text not null check (action in ('start','skip','more','done'))
   )
 `);
-
-// TODO: create indexes if not exists?
-
-db.execute(`drop table if exists routine`);
 db.execute(`
-  create table routine
-  ( created_at timestamp not null default current_timestamp
-  , id integer not null primary key autoincrement
-  , event text not null check (id <> '')
-  , desc text not null
-  , duration int not null check (duration > 0)
-  , days text not null
-  , start int not null check (start between 0 and 23)
-  , end int not null check (end between 0 and 23)
+  create index if not exists log_idx on log
+  ( created_at
+  , action
   )
 `);
 
 // TODO: parse .config/routine.csv
 const routines = [
   {
-    event: 'exercise-1',
-    desc: 'Did you exercise and listen to an audiobook? (1)',
+    event: "exercise-1",
+    desc: "Did you exercise and listen to an audiobook? (1)",
     duration: 25,
-    days: 'MTWRFAU',
+    days: "MTWRFAU",
     start: 4,
     end: 10,
   },
   {
-    event: 'exercise-2',
-    desc: 'Did you exercise and listen to an audiobook? (2)',
+    event: "exercise-2",
+    desc: "Did you exercise and listen to an audiobook? (2)",
     duration: 25,
-    days: 'MTWRFAU',
+    days: "MTWRFAU",
     start: 10,
     end: 14,
   },
 ];
 
-for (const routine of routines) {
-  const cols = Object.keys(routine);
-  db.query(`insert into routine (${cols.join(`,`)}) values (${cols.map(k=>':'+k).join(`,`)})`, routine);
-}
+const routinesKeys = ["id", ...Object.keys(routines?.[0] ?? {})]; // TODO: kludge
+const routinesVals = routines.map(
+  (o: Record<string, unknown>, id) =>
+    routinesKeys.map((k) => `'${{ id, ...o }[k]}'`),
+);
 
-switch (help?`help`:command) {
+const next = () => {
+  const routinesVals_ = routinesVals.map((x) => `(${x.join(",")})`).join(",");
+  const [[created_at, event, action] = []] = db.query(`
+    select *
+    from (values ${routinesVals_}) as r (${routinesKeys.join(",")})
+    left join log l on l.event = r.event and l.action = 'start'
+    where strftime('%H', current_timestamp) between r.start and r.end
+      and 0 < instr(days, case strftime('%w',current_timestamp) when 0 then 'U' when 1 then 'M' when 2 then 'T' when 3 then 'W' when 4 then 'R' when 5 then 'F' when 6 then 'A' end)
+      and event not in (
+        select event
+        from log
+        where created_at > datetime(current_timestamp, 'start of day') 
+          and (action = 'done' or action = 'skip' and created_at > datetime(current_timestamp,'-40 minutes'))
+      )
+    order by l.created_at asc nulls last, r.id asc
+    limit 1
+  `) ?? [];
+  db.query(
+    `insert into log (event, action) values (?, 'start')`,
+    [event as string],
+  );
+  return created_at && event && action && { created_at, event, action };
+};
 
-  case "cli":
+switch (help ? `help` : command) {
+  case "cli": {
+    let row = next();
+    console.log(row);
     for await (const keypress of readKeypress()) {
-      if (keypress.ctrlKey && keypress.key === 'c') break;
+      if (keypress.ctrlKey && keypress.key === "c") break;
+      if (!row) break;
       const keymap = {
         d: `done`,
         s: `skip`,
         n: `more`,
       };
       // TODO: make sure that start of day is current timezone
-      const [row] = db.query(`
-        select
-          max()
-        from routine r
-        left join log l on r.event = l.event
-        where l.created_at > datetime(current_timestamp, 'start of day') 
-          and strftime('%H', current_timestamp)::integer between r.start and r.end
-          and 0 < instr(r.days, case strftime('%w',current_timestamp) when 0 then 'U' when 1 then 'M' when 2 then 'T' when 3 then 'W' when 4 then 'R' when 5 then 'F' when 6 then 'A' end)
-        group by r.event
-        order by 1, 2
-        limit 1
-      `);
-      if (!row) break;
       switch (keypress.key) {
         case "done":
           // TODO
@@ -126,21 +127,29 @@ switch (help?`help`:command) {
           console.log(keypress);
           break;
       }
+      row = next();
+      console.log(row);
     }
     break;
+  }
 
   case "stats":
     // TODO
     break;
 
-  case "annoy":
+  case "annoy": {
     // TODO: create docs for how to run this inline like `watch -n 2 -c deno annoy.ts`
-    const [t,event,action] = db.query(`select created_at, event, action from log order by created_at desc limit 1`) ?? [];
-    // TODO: if no events exist, try to start a new one
-    console.log(t,event,action);
+    // TODO: don't beep if something has been started and its routine still might be active or more time was allottedd
+    const [t, event, action] = db.query(
+      `select created_at, event, action from log order by created_at desc limit 1`,
+    ) ?? [];
+    console.log(t, event, action);
     // TODO: do something more extreme than a beep if nowify hasn't been run lately
-    await Deno.run({ cmd: `afplay -v 5 /System/Library/Sounds/Morse.aiff`.split(` `) }).status();
+    await Deno.run({
+      cmd: `afplay -v 5 /System/Library/Sounds/Morse.aiff`.split(` `),
+    }).status();
     break;
+  }
 
   case "export":
     // TODO
@@ -152,6 +161,7 @@ switch (help?`help`:command) {
 
   default:
     console.log(`invalid command: '${command}'`);
+    // falls through
   case "":
   case "help":
     console.log([
